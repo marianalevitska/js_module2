2a6ad773ece7cf5d3e7ef5d61891b8a938804c6c
Mariana <mariamna5204@gmail.com>
2022-04-09 23:26:22 +0300

    11

--- module_4.js
+++ module_4.js
@@ -477,3 +477,13 @@
 
 // };
 // // Change code above this line
+
+
+// Доповни функцію getActiveUsers(users) таким чином, щоб вона повертала масив активних користувачів, значення властивості isActive яких - true.
+// // Change code below this line
+// const getActiveUsers = (users) => {
+//     return users.filter(user => user.isActive === true);
+// };
+// // Change code above this line
+
+
