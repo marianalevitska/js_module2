44b33ce9a6fca1bfc42931cd790c5dfbca18823f
Mariana <mariamna5204@gmail.com>
2022-04-10 17:09:28 +0300

    Update module_4.js

--- module_4.js
+++ module_4.js
@@ -696,6 +696,23 @@
 // console.log(inAscendingScoreOrder);
 // console.log(inDescendingScoreOrder);
 
+// Доповни код таким чином, щоб у змінній ascendingReleaseDates вийшла копія масиву releaseDates, відсортована за зростанням, а у змінній alphabeticalAuthors - копія масиву імен авторів authors, відсортована за алфавітом.
+
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//     "Tanith Lee",
+//     "Bernard Cornwell",
+//     "Robert Sheckley",
+//     "Fyodor Dostoevsky",
+// ];
+// // Change code below this line
+
+// const ascendingReleaseDates = [...releaseDates].sort();
+
+// const alphabeticalAuthors = [...authors].sort();
+// console.log(ascendingReleaseDates);
+// console.log(alphabeticalAuthors);
+
 
 
 
