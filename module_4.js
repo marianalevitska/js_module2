4e903e140af24fcd94d9f12655e89b7f902c12f9
Mariana <mariamna5204@gmail.com>
2022-04-10 17:14:00 +0300

    Update module_4.js

--- module_4.js
+++ module_4.js
@@ -715,13 +715,28 @@
 
 // Онлайн бібіліотеці необхідно відображати книги, відсортовані за датою видання, за її зростанням або спаданням.Доповни код таким чином, щоб у змінній ascendingReleaseDates вийшла копія масиву releaseDates, відсортована за зростанням, а у змінній descendingReleaseDates - копія, відсортована за спаданням.
 
-const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
-// Change code below this line
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// // Change code below this line
 
-const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
 
-const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
-console.log(ascendingReleaseDates);
-console.log(descendingReleaseDates);
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+// console.log(ascendingReleaseDates);
+// console.log(descendingReleaseDates);
+
+// Онлайн бібіліотеці необхідно відображати книги, відсортовані за автором, в алфавітному і зворотному алфавітному порядку.Доповни код таким чином, щоб у змінній authorsInAlphabetOrder вийшла копія масиву authors, відсортована за алфавітом, а у змінній authorsInReversedOrder - копія, відсортована у зворотному алфавітному порядку.
+
+// const authors = [
+//     "Tanith Lee",
+//     "Bernard Cornwell",
+//     "Robert Sheckley",
+//     "Fyodor Dostoevsky",
+//     "Howard Lovecraft",
+// ];
+// // Change code below this line
+
+// const authorsInAlphabetOrder = [...authors].sort((a, b) => a.localeCompare(b));
+
+// const authorsInReversedOrder = [...authors].sort((a, b) => b.localeCompare(a));
 
 
