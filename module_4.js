310d66251928b2696fdd8b28f9f0ed691234e3a6
Mariana <mariamna5204@gmail.com>
2022-04-10 16:11:52 +0300

    reduce

--- module_4.js
+++ module_4.js
@@ -552,3 +552,24 @@
 // console.log(totalScore);
 // console.log(averageScore);
 
+// Ігровому сервісу необхідний функціонал підрахунку середнього часу, проведеного в іграх.Доповни код таким чином, щоб у змінній totalPlayTime вийшло загальний ігровий час з масиву playtimes.
+
+// const players = {
+//     mango: 1270,
+//     poly: 468,
+//     ajax: 710,
+//     kiwi: 244
+// };
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// // Change code below this line
+
+// const totalPlayTime = playtimes.reduce((total, time) => {
+//     return total + time;
+// }, 0);
+
+// // Change code above this line
+// const averagePlayTime = totalPlayTime / playtimes.length;
+// console.log(playtimes);
+// console.log(totalPlayTime);
+// console.log(averagePlayTime);
+
