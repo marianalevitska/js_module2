5a3068492222ff7db94e5f4523e27b2247f7b7ff
Mariana <mariamna5204@gmail.com>
2022-04-10 16:15:24 +0300

    Update module_4.js

--- module_4.js
+++ module_4.js
@@ -573,3 +573,17 @@
 // console.log(totalPlayTime);
 // console.log(averagePlayTime);
 
+// Нашому сервісу необхідно розрахувати середній час, проведений в одній грі для кожного гравця, і отримати загальну суму цих значень часу.Розрахувати час для кожного з гравців можна, розділивши його час(властивість playtime) на кількість ігор(властивість gamesPlayed).
+
+const players = [
+    { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+    { name: "Poly", playtime: 469, gamesPlayed: 2 },
+    { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+    { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+// Change code below this line
+
+const totalAveragePlaytimePerGame = players.reduce((total, time) => {
+    return total + time.playtime / time.gamesPlayed;
+}, 0);
+console.log(totalAveragePlaytimePerGame);
