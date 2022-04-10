2fdf10aab9f7322f34d870dc20f78402b375f28e
Mariana <mariamna5204@gmail.com>
2022-04-10 17:06:33 +0300

    Update module_4.js

--- module_4.js
+++ module_4.js
@@ -673,3 +673,29 @@
 // };
 // // Change code above this line
 
+// SORT()
+// const students = [
+//     { name: "Манго", score: 83 },
+//     { name: "Полі", score: 59 },
+//     { name: "Аякс", score: 37 },
+//     { name: "Ківі", score: 94 },
+// ];
+
+// const inAscendingScoreOrder = students.sort(
+//     (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
+// );
+
+// const inDescendingScoreOrder = students.sort(
+//     (firstStudent, secondStudent) => secondStudent.score - firstStudent.score
+// );
+
+// const inAlphabeticalOrder = students.sort((firstStudent, secondStudent) =>
+//     firstStudent.name.localeCompare(secondStudent.name)
+// );
+// console.log(inAlphabeticalOrder);
+// console.log(inAscendingScoreOrder);
+// console.log(inDescendingScoreOrder);
+
+
+
+
