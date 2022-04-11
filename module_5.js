c914f1adab60e19aee925af553ae9cdb3e3baf2a
Mariana <mariamna5204@gmail.com>
2022-04-11 19:28:50 +0300

    Update module_5.js

--- module_5.js
+++ module_5.js
@@ -24,3 +24,24 @@
 
 // petya.showThis(); // {username: "Petya", showThis: ƒ, showName: ƒ}
 // petya.showName(); // 'Petya'
+
+
+// Виконай рефакторинг методів об'єкта pizzaPalace, розставивши відсутні this в місцях звернення до властивостей і методів об'єкта.
+const pizzaPalace = {
+    pizzas: ["Supercheese", "Smoked", "Four meats"],
+    // Change code below this line
+    checkPizza(pizzaName) {
+        return this.pizzas.includes(pizzaName);
+    },
+    order(pizzaName) {
+        const isPizzaAvailable = this.checkPizza(pizzaName);
+
+        if (!isPizzaAvailable) {
+            return `Sorry, there is no pizza named «${pizzaName}»`;
+        }
+
+        return `Order accepted, preparing «${pizzaName}» pizza`;
+    },
+    // Change code above this line
+};
+
