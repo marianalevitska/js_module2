b07b0bf29ea27a4430f284465906423dc877d411
Mariana <mariamna5204@gmail.com>
2022-04-11 17:53:42 +0300

    Update module_5.js

--- module_5.js
+++ module_5.js
@@ -1,13 +1,26 @@
-const bookShelf = {
-    authors: ["Бернард Корнуелл", "Роберт Шеклі"],
-    getAuthors() {
-        return this.authors;
-    },
-    addAuthor(authorName) {
-        this.authors.push(authorName);
-    },
-};
+// const bookShelf = {
+//     authors: ["Бернард Корнуелл", "Роберт Шеклі"],
+//     getAuthors() {
+//         return this.authors;
+//     },
+//     addAuthor(authorName) {
+//         this.authors.push(authorName);
+//     },
+// };
 
-console.log(bookShelf.getAuthors()); // ["Бернард Корнуелл", "Роберт Шеклі"]
-bookShelf.addAuthor("Ли Танит");
-console.log(bookShelf.getAuthors());
+// console.log(bookShelf.getAuthors()); // ["Бернард Корнуелл", "Роберт Шеклі"]
+// bookShelf.addAuthor("Ли Танит");
+// console.log(bookShelf.getAuthors());
+
+// const petya = {
+//     username: "Petya",
+//     showThis() {
+//         console.log(this);
+//     },
+//     showName() {
+//         console.log(this.username);
+//     },
+// };
+
+// petya.showThis(); // {username: "Petya", showThis: ƒ, showName: ƒ}
+// petya.showName(); // 'Petya'
