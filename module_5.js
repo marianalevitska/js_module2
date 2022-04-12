d1d566cde0a8a5a5acf5ef59bb93f5fe50cddbcb
Mariana <mariamna5204@gmail.com>
2022-04-13 01:18:07 +0300

    Update module_5.js

--- module_5.js
+++ module_5.js
@@ -218,3 +218,37 @@
 //     // Change code above this line
 // }
 
+
+// Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. Клас очікує тільки один аргумент - початковий масив товарів, який записується у властивість items об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getItems() - повертає масив поточних товарів у властивості items об'єкта, який викликає цей метод.
+// addItem(newItem) - приймає новий товар newItem і додає його в масив товарів у властивості items об'єкта, який викликає цей метод.
+// removeItem(itemToRemove) - приймає товар itemToRemove і видаляє його з масиву товарів у властивості items об'єкта, який викликає цей метод.
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести.Будь ласка, нічого там не змінюй.
+
+// class Storage {
+//     constructor(items) {
+//         this.items = items;
+//     }
+//     getItems() {
+//         return this.items;
+//     }
+//     addItem(newItem) {
+//         return this.items.push(newItem);
+//     }
+//     removeItem(itemToRemove) {
+//         return this.items.splice(1, 1);
+//     }
+// }
+
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
