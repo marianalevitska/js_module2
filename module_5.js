f844daa797347fce67e65833d105cf893ead2962
Mariana <mariamna5204@gmail.com>
2022-04-13 00:39:22 +0300

    Update module_5.js

--- module_5.js
+++ module_5.js
@@ -80,3 +80,31 @@
 // console.log(customer.getBalance()); // 19750
 // console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
 
+// Тестувальники знайшли баги в коді сервісу зберігання історії замовлень їжі.Тобі необхідно виправити їх, правильно розставивши this в методах об'єкта historyService, щоб методи почали працювати правильно.
+
+
+// const historyService = {
+//     orders: [
+//         { email: "jacob@hotmail.com", dish: "Burrito" },
+//         { email: "solomon@topmail.net", dish: "Burger" },
+//         { email: "artemis@coldmail.net", dish: "Pizza" },
+//         { email: "solomon@topmail.net", dish: "Apple pie" },
+//         { email: "jacob@hotmail.com", dish: "Taco" },
+//     ],
+//     // Change code below this line
+//     getOrdersLog() {
+//         return this.orders
+//             .map(order => `email: ${order.email} dish: ${order.dish}`)
+//             .join(" - ");
+//     },
+//     getEmails() {
+//         const emails = this.orders.map(order => order.email);
+//         const uniqueEmails = new Set(emails);
+//         return [...uniqueEmails];
+//     },
+//     getOrdersByEmail(email) {
+//         return this.orders.filter(order => order.email === email);
+//     },
+//     // Change code above this line
+// };
+
