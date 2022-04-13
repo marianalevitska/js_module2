779d92a215646b742a3f0489d10d3d3f430465e8
Mariana <mariamna5204@gmail.com>
2022-04-13 14:48:55 +0300

    Update module_5.js

--- module_5.js
+++ module_5.js
@@ -432,3 +432,43 @@
 // }
 
 
+
+// Виконай рефакторинг класу Car.Додай публічну статичну властивість MAX_PRICE зі значенням 50000 - максимально допустима ціна автомобіля.
+
+// Додай сетеру price перевірку значення параметра newPrice, що передається.Якщо воно більше за MAX_PRICE, сеттер нічого не робить, а якщо менше або дорівнює, то перезаписує ціну автомобіля.
+
+// class Car {
+//     // Change code below this line
+
+//     static MAX_PRICE = 50000;
+
+//     // MAX_PRICE;
+//     #price;
+
+//     constructor({ price }) {
+//         this.#price = price;
+//     }
+
+//     get price() {
+//         return this.#price;
+//     }
+
+//     set price(newPrice) {
+//         if (newPrice > Car.MAX_PRICE) {
+//             return;
+//         }
+//         this.#price = newPrice;
+//     }
+//     // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+
