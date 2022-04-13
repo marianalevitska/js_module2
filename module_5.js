6c102ae6703e15a0f3089f802c1f359f353e826e
Mariana <mariamna5204@gmail.com>
2022-04-13 14:53:46 +0300

    Update module_5.js

--- module_5.js
+++ module_5.js
@@ -472,3 +472,32 @@
 // console.log(audi.price); // 49000
 
 
+// Додай класу Car публічний статичний метод checkPrice(price), що приймає ціну автомобіля.Метод повинен порівняти значення параметра price і приватної статичного властивості MAX_PRICE.
+
+// Якщо ціна автомобіля перевищує максимальну, метод повинен повернути рядок "Error! Price exceeds the maximum".
+// В іншому випадку метод повинен повернути рядок "Success! Price is within acceptable limits".
+// Під оголошенням класу ми додали ініціалізацію екземпляра і виклики методів, щоб показати, як буде використовуватися метод checkPrice(price).
+
+
+// class Car {
+//     static #MAX_PRICE = 50000;
+//     // Change code below this line
+//     static checkPrice(price) {
+//         if (price > Car.#MAX_PRICE) {
+//             return "Error! Price exceeds the maximum";
+//         }
+//         return "Success! Price is within acceptable limits";
+//     }
+//     // Change code above this line
+//     constructor({ price }) {
+//         this.price = price;
+//     }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+
