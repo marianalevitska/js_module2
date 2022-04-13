3fb45bf3be924088e22704c8e8ddc2ee221867ba
Mariana <mariamna5204@gmail.com>
2022-04-13 14:13:26 +0300

    Update module_5.js

--- module_5.js
+++ module_5.js
@@ -296,3 +296,26 @@
 // builder.padBoth("=");
 // console.log(builder.getValue()); // "=^.^="
 
+// Виконай рефакторинг класу Car таким чином, щоб властивість brand була приватною, і додай два методи для публічного інтерфейсу, для читання і зміни цієї властивості.
+
+//     getBrand() - повертає значення приватної властивості brand.
+//         changeBrand(newBrand) - змінює значення приватної властивості brand на newBrand.
+
+// class Car {
+//     // Change code below this line
+//     #brand;
+//     constructor({ brand, model, price }) {
+//         this.#brand = brand;
+//         this.model = model;
+//         this.price = price;
+//     }
+//     getBrand() {
+//         return this.#brand;
+//     }
+//     changeBrand(newBrand) {
+//         this.#brand = newBrand;
+//     }
+
+//     // Change code above this line
+// }
+
