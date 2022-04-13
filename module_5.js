2a99ad66dedb780d2c6c610eb80bf5baa82adab6
Mariana <mariamna5204@gmail.com>
2022-04-13 14:15:36 +0300

    Update module_5.js

--- module_5.js
+++ module_5.js
@@ -319,3 +319,35 @@
 //     // Change code above this line
 // }
 
+// Виконай рефакторинг класу Storage, зробивши властивість items приватною.
+
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести.Будь ласка, нічого там не змінюй.
+
+// class Storage {
+//     // Change code below this line
+//     #items;
+//     constructor(items) {
+//         this.#items = items;
+//     }
+
+//     getItems() {
+//         return this.#items;
+//     }
+
+//     addItem(newItem) {
+//         this.#items.push(newItem);
+//     }
+
+//     removeItem(itemToRemove) {
+//         this.#items = this.#items.filter(item => item !== itemToRemove);
+//     }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+
