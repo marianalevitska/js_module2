e3af413a3b147741b37ea027e96158867d8127aa
Mariana <mariamna5204@gmail.com>
2022-04-13 11:31:44 +0300

    Update module_5.js

--- module_5.js
+++ module_5.js
@@ -252,3 +252,47 @@
 // storage.removeItem("Prolonger");
 // console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
+// Напиши клас StringBuilder, який приймає один параметр initialValue - довільний рядок, який записується у властивість value об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getValue() - повертає поточне значення властивості value.
+//     padEnd(str) - отримує параметр str(рядок) і додає його в кінець значення властивості value об'єкта, який викликає цей метод.
+// padStart(str) - отримує параметр str(рядок) і додає його на початок значення властивості value об'єкта, який викликає цей метод.
+// padBoth(str) - отримує параметр str(рядок) і додає його на початок і в кінець значення властивості value об'єкта, який викликає цей метод.
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести.Будь ласка, нічого там не змінюй.
+
+// str + massive ()
+// builder - is an object, so [...abbs, str] isn't suitable
+// class StringBuilder {
+
+//     constructor(initialValue) {
+//         this.value = initialValue;
+//     }
+
+//     getValue() {
+//         return this.value;
+//     }
+//     padEnd(str) {
+//         this.value = this.value + str;
+//     }
+//     padStart(str) {
+//         this.value = str + this.value;
+//     }
+//     padBoth(str) {
+//         this.value = str + this.value + str;
+//     }
+
+// }
+
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
